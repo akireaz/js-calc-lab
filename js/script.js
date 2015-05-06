06a5b865b624049a0dc913bb4a5ff8e10455ca5f
Valeria Palma <akiretta@gmail.com>
2015-05-06 12:54:02 +0100

    takethis

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var numbers = document.getElementsByClassName("number")
+console.log(numbers)
+var display = document.getElementById("display")
+console.log(display)
+var clear = document.getElementsByClassName("clear")
+var operator = document.getElementsByClassName("operator")
+var firstNumber = '';
+var secondNumber = '';
+var calculator = '';
+
+
+
+
+var para = document.getElementById("para")
+
+para.addEventListener("click", function(){
+    alert("hello")
+});
+
+for (var i=0; i<number.length; i++) {
+
+ number[i].addEventListener('click', function(e){
+   consolo.log(this value) ;
+ });
+
+
+
+
